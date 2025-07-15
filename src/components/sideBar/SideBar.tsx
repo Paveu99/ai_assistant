d60dca2435f30a9062b9c79f0062af9b17552890
Paweł Jarecki <paweljarecki10@gmail.com>
2025-07-15 21:49:01 +0200

    New view of right slider menu - Now there is a place to set the overall theme of the application as well as information about the owner and his Github repository

--- src/components/sideBar/SideBar.tsx
+++ src/components/sideBar/SideBar.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import { useState } from 'react';
-import { Drawer, List, ListItem, ListItemText, IconButton, SvgIcon } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+import { Drawer, List, ListItem, ListItemText, IconButton, SvgIcon, styled, FormControlLabel, Button, Typography } from '@mui/material';
+import SettingsIcon from '@mui/icons-material/Settings';
 import Link from 'next/link';
+import { LightDarkSwitch } from '../lightDarkSwitch/LightDarkSwitch';
+import { useTheme } from '@/context/ThemeContext';
 
 const navLinks = [
     { href: '/', label: 'Home' },
@@ -15,13 +17,20 @@ const navLinks = [
 ];
 
 export default function SideBar() {
-    const [open, setOpen] = useState(true);
+    const [open, setOpen] = useState<boolean>(false);
+    const [open2, setOpen2] = useState<boolean>(false);
+    const { currentTheme, toggleTheme } = useTheme();
 
     return (
         <>
-            <SvgIcon>
-                <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" onClick={() => setOpen(true)} />
-            </SvgIcon>
+            <button onClick={() => setOpen(true)} >
+                <SvgIcon>
+                    <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+                </SvgIcon>
+            </button>
+            <button onClick={() => setOpen2(true)}>
+                <SettingsIcon />
+            </button>
             <Drawer anchor="left" open={open} onClose={() => setOpen(false)}>
                 <List sx={{ width: 250 }}>
                     {navLinks.map(({ href, label }) => (
@@ -32,6 +41,41 @@ export default function SideBar() {
                         </ListItem>
                     ))}
                 </List>
+            </Drawer>
+            <Drawer anchor='right' open={open2} onClose={() => setOpen2(false)}>
+                <div className="w-[250px] h-full flex flex-col justify-between">
+                    <List>
+                        <ListItem>
+                            <FormControlLabel
+                                control={<LightDarkSwitch
+                                    sx={{ m: 1 }}
+                                    checked={currentTheme === 'dark'}
+                                    onChange={toggleTheme}
+                                />}
+                                label={currentTheme === "light" ? "Light" : "Dark"}
+                            />
+                        </ListItem>
+                    </List>
+                    <div className="p-4 flex flex-col gap-2">
+                        <Button
+                            fullWidth
+                            variant="outlined"
+                            component="a"
+                            href="https://github.com/Paveu99"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Owner's repo
+                        </Button>
+                        <Typography
+                            variant="caption"
+                            color="textSecondary"
+                            align="center"
+                        >
+                            Application is courtesy of Pawel Jarecki. All rights reserved.
+                        </Typography>
+                    </div>
+                </div>
             </Drawer>
         </>
     );
