e01fe0fcba7713f212e1ba2d973fa0620be4e7c2
Paweł Jarecki <paweljarecki10@gmail.com>
2025-07-15 21:46:40 +0200

    Context for whole app usage

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { useToggleTheme } from '@/hooks/useToggleTheme';
+import { createContext, useContext, useState, ReactNode } from 'react';
+
+type Theme = 'light' | 'dark';
+
+interface ThemeContextType {
+    currentTheme: Theme;
+    toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+    const { currentTheme, toggleTheme } = useToggleTheme();
+
+    if (!currentTheme) return null;
+
+    return (
+        <ThemeContext.Provider value={{ currentTheme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export const useTheme = (): ThemeContextType => {
+    const context = useContext(ThemeContext);
+    if (!context) throw new Error('useTheme must be used within ThemeProvider');
+    return context;
+};
